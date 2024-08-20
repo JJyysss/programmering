f6e7b1ac61ff97d963626833e6ceee6a56e409d6
JJyysss <jiayishi999@gmail.com>
2024-08-20 09:29:08 +0200

    add filer

--- 02 web template css tricks/index.js
+++ 02 web template css tricks/index.js
@@ -1,6 +1,7 @@
 
-let currentPage =1
+let currentPage =3
 let pages // array med alle elementer med class = page
+let colors = ['red','green','blue','lightblue','pink','skyblue','lightgreen','lightgrey','purple','white','darkblue','darkgreen','yellow']
 
 function setup(){
     console.log('p5.js er loaded')
@@ -8,6 +9,16 @@ function setup(){
     pages = selectAll('.page')
     // nu kan man se at pages er blevet til en liste med alle class = page ting
     console.log(pages.length)
+
+    // lave en masse div'er vi kommer ind i page 3
+    for(c of colors){
+        //console.log(c)
+        let div=createDiv()
+        div.style('background-color',c)
+        select('#page3').child(div)
+        
+
+    }
 
 }
 
