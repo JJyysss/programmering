9993b57c437310a5aabcc4e9459bb28af015c018
JJyysss <jiayishi999@gmail.com>
2024-08-23 11:06:38 +0200

    fixed template js

--- 02 web template css tricks/index.js
+++ 02 web template css tricks/index.js
@@ -2,11 +2,29 @@
 let currentPage =3
 let pages // array med alle elementer med class = page
 let colors = ['red','green','blue','lightblue','pink','skyblue','lightgreen','lightgrey','purple','white','darkblue','darkgreen','yellow']
+let menuNumber = 1
+let menuItems // array med alle menupunkterne
+
 
 function setup(){
     console.log('p5.js er loaded')
     select('#page' + currentPage).addClass('visible')
+    select('#menu' + menuNumber).addClass('active')
+
     pages = selectAll('.page')
+    menuItems = selectAll('.menuitem')
+
+    for( m of menuItems ){
+        m.mousePressed( function(e){
+            // e.target er selve html div'en
+            console.log(e.target.id)
+            // slice -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            shiftPage(nr)
+
+        } )
+    }
+
     // nu kan man se at pages er blevet til en liste med alle class = page ting
     console.log(pages.length)
 
@@ -16,7 +34,7 @@ function setup(){
         let div=createDiv()
         div.style('background-color',c)
         select('#page3').child(div)
-        
+
 
     }
 
@@ -37,6 +55,9 @@ function shiftPage(num){
     select("#page" + currentPage).removeClass('visible')
     currentPage = num
     select("#page" + currentPage).addClass('visible')
+    select("#menu" + menuNumber).removeClass('active')
+    menuNumber = num
+    select("#menu" + menuNumber).addClass('active')
 
 }
 
