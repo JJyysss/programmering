296c5c7933d0881e678beaad2819660aa810790a
JJyysss <jiayishi999@gmail.com>
2025-01-03 00:57:18 +0100

    up

--- jul homework/class.js
+++ jul homework/class.js
@@ -72,14 +72,6 @@ class Being {
     const introElement = document.getElementById('intro');
     const alienSpeakElement = document.getElementById('alien-speak');
   
-    const characters = [
-      new Human('Alice', 30, 'the USA'),
-      new Human('Ben', 40, 'Russia'),
-      new Human('Charile', 25, 'China'),
-      new Alien('Zorg', 300, 'Mars'),
-      new Alien('Blorp', 150, 'Venus'),
-      new Alien('Xarg', 400, 'Pluto'),
-    ];
   
     characters.forEach(character => createCard(character, container, introElement,alienSpeakElement ));
   }
