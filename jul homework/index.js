f9fe177d1f74983ccd085c9df0dc25c62d93b5ed
JJyysss <jiayishi999@gmail.com>
2025-01-03 01:22:09 +0100

    up

--- jul homework/index.js
+++ jul homework/index.js
@@ -1,26 +1,4 @@
-function setup() {
-    
-    const container = document.getElementById('class-container');
-    const introElement = document.getElementById('intro');
-    const alienTalkElement = document.getElementById('alien-speak');
-  
-    const characters = [
-      new Human('Alice', 28, 'the USA'),
-      new Human('Amber', 25, 'Russia'),
-      new Human('Jellycat', 80, 'London'),
-      new Human('doraemon', 18, 'Japan'),
-      new Alien('Hello Kitty', 300, 'Hello'),
-      new Alien('Pikachu', 150, 'Pokémon'),
-      new Alien('Labubu', 400, 'Pluto'),
-    ];
-  
-    
-  for (const character of characters) {
-    createCard(character, container, introElement, alienTalkElement);
-  }
-  }
-  
-  setup();
-  
+let classContainer
+
 
   
